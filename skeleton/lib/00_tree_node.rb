e6522620516b4467375ac439d4cfb35aebe09c6a
Michael Gastello <michaelgastello@aol.com>
2023-01-27 14:04:23 -0500

    added initialize method

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,13 @@
 class PolyTreeNode
+    attr_reader :parent, :children, :value
 
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=
+        parent = nil
+    end
 end
